d1005717c8ca0e4b38b9f7a8e2cfdaaede1092b1
Alex <alex@mindscape.co.nz>
2017-04-18 15:40:30 +1200

    update install script

--- install.sh
+++ install.sh
@@ -11,5 +11,9 @@ npm install n
 npm install total.js
 npm install adonis-cli -g
 adonis new yardstick
+cp yardstick-changes/app/Http/kernel.js yardstick/app/Http/kernel.js
+cp yardstick-changes/app/Http/routes.js yardstick/app/Http/routes.js
 npm install sails -g
 sails new test-project
+cp test-project-changes/api/controllers/ApiController.js test-project/api/controllers/ApiController.js
+cp test-project-changes/config/routes.js test-project/config/routes.js

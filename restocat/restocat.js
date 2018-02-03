b358cc731fba8441befb6555b61dde87beed7bc8
Maksim Chetverikov <ma.chetverikov@gmail.com>
2018-02-03 18:40:40 +0800

    add restocat

---
+++ restocat/restocat.js
@@ -0,0 +1,13 @@
+const Restocat = require('restocat');
+const rest = new Restocat({
+  collectionsGlob: [
+    'restocat/collections/**/collection.json'
+  ]
+});
+
+rest.locator.registerInstance('definedRoutes', {index: '/'});
+
+rest.createServer().listen(8000)
+  .then(() => console.log('Restocat listen on 8000'))
+  .catch(reason => console.error(reason));
+

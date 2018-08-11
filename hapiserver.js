6f1a800fde50bf18b0d12e06685b8b17fa6f2fdf
Sejin Jeon <jinaidy93@gmail.com>
2018-08-11 23:10:02 +0000

    Change code of hapiserver.js into a changed grammar
    See this: https://github.com/hapijs/hapi/issues/3692

--- hapiserver.js
+++ hapiserver.js
@@ -3,8 +3,7 @@
 const Hapi = require('hapi');
 
 // Create a server with a host and port
-const server = new Hapi.Server();
-server.connection({ 
+const server = new Hapi.Server({
     host: 'localhost', 
     port: 8000 
 });
